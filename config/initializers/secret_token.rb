f72ec8dcab6a35a08ccbbfe76c6784682ba7ec0d
DJ Tyree <djtyree@alumni.clemson.edu>
2013-04-15 16:42:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppStore::Application.config.secret_token = 'cb3708b76cea8648cd415b1da227085d1a656b503d0a7ed0c7cec8acf6bcaec1fed0e579392d3a27e1761332eca020a3b6dc07179cf333ce362b1d59a9e16caa'
